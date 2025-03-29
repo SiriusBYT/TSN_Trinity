af8e0616ac0810088c648a7cbaab8221c85f840e
Ascellayn <root.siriusb@gmail.com>
2025-03-29 20:31:32 +0100

    [CLIENT] Clear console before importing Trinity

--- T_Client.py
+++ T_Client.py
@@ -1,7 +1,5 @@
 # This file imports Trinity and runs its client.
+from TSN_Abstracter import *; Log.Clear();
 from Trinity import *;
-import TSN_Abstracter;
 
-Log.Clear();
-Config.Logging["File"] = False; # Disable Log Files
 Trinity_Client(Address = ("localhost", 1407), Shell=True);
