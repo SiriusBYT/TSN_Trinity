cd90effcb39baac8ef327078f4b80c4ad283d023
Ascellayn <root.siriusb@gmail.com>
2025-03-29 20:30:04 +0100

    Make Client debugging less painful by using Log.Clear()

--- T_Client.py
+++ T_Client.py
@@ -1,4 +1,7 @@
 # This file imports Trinity and runs its client.
 from Trinity import *;
+import TSN_Abstracter;
 
+Log.Clear();
+Config.Logging["File"] = False; # Disable Log Files
 Trinity_Client(Address = ("localhost", 1407), Shell=True);
