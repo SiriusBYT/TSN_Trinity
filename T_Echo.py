830a8364e3bc77f11d498e0cdb41492ae74d0ff4
Ascellayn <root.siriusb@gmail.com>
2025-03-30 14:40:09 +0200

    Start of writing relayer, Relay JSON update, Trinity_Socket slight simplification

--- T_Echo.py
+++ T_Echo.py
@@ -8,10 +8,9 @@ def Echo(Client: Trinity_Socket, Command: str) -> bool:
 
 # Example Routine Function
 def Echo_Routine(Client: Trinity_Socket):
+    time.sleep(10);
     while (Client.Connected):
+        Client.Send(f"Routine Message!");
         time.sleep(10);
-        while (Client.Listen):
-            #Client.Send(f"Routine Message!");
-            time.sleep(10);
 
 Trinity_Server(Processor=Echo, Routine=Echo_Routine, Type="Endpoint");
--- Trinity.py
+++ Trinity.py
@@ -9,44 +9,6 @@ from websockets.sync.client import connect;
 import dotenv;
 import time, math;
 
-
-Version: str = "a250329";
-# Turn these into a dict later that can be read/modified via JSON
-Packet_Size: int = 8192;
-Server_Running: bool = True;
-Server_Max_Communication_Errors: int = 3;
-Server_Max_Communication_Length: int = 3600;
-
-""" Make the IDE less angry because of the type safe functions/variables below. """
-class Trinity_Server: pass;
-Connected_Clients: list[Trinity_Server] = [];
-
-def Trinity_Relayer(Client: Trinity_Server, Command: str) -> bool:
-    Client.Send(Command);
-    return True;
-
-def Trinity_Routine() -> None:
-    while (Server_Running):
-        Log.Carriage(f"Connected Clients: {len(Connected_Clients)} // {[str(Client) for Client in Connected_Clients]}");
-        time.sleep(1);
-
-""" MEGA Cursed Syntax I'm aware, these functions are ran once and gives us the Keys for the server.
-Gives us the variables "Key_Private" and "Key_Public" which are going to be used to secure communication.
-It's better to generate a new key for each client security wise but this is taxing on the server and it doesn't really
-matter anyways since the bigger problem is storing the data received rather than the currently on-going communications"""
-""" # No longer required due to switch to websockets exclusively
-@lambda _: _()
-def Key_Private():
-    Log.Info("A Private Key is being generated for this session. Please hold.");
-    return Cryptography.Generate_Private();
-
-@lambda _: _()
-def Key_Public():
-    Log.Info("A Public Key is being generated for this session. Please hold.");
-    return Cryptography.Generate_Public(Key_Private);
-"""
-
-
 class Trinity_Socket:
     def __init__(
             self, 
@@ -58,7 +20,6 @@ class Trinity_Socket:
         ):
         """NOTE: Processor is a FUNCTION! For some reason adding "function" to declare we want to accept a function,
         doesn't FUCKING WORK because Python is retarded or something. This is hateful. We're at the mercy of the user not fucking up."""
-        Connected_Clients.append(self);
         self.Socket = WebSocket;
         self.Address: str = f"{self.Socket.remote_address[0]}:{self.Socket.remote_address[1]}" if (WebSocket != None) else Address;
 
@@ -112,7 +73,6 @@ class Trinity_Socket:
         except:
             Misc.Void();
         S_Close.OK();
-        Connected_Clients.remove(self);
 
 
     def Send_Code(self, Message: str) -> bool:
@@ -165,12 +125,16 @@ class Trinity_Server(Trinity_Socket):
 class Trinity_Client(Trinity_Socket):
     def Configuration(self) -> None:
         self.Ping = time.monotonic()*1000;
-        self.Socket = connect(self.Address);
+        try:
+            self.Socket = connect(self.Address);
+        except:
+            self.Terminate();
         self.Ping = math.floor(((time.monotonic()*1000) - self.Ping));
         self.Interactive = self.kwargs["Shell"];
         Log.Info(f"Connected to {self.Address} with a latency of {self.Ping}ms.");
 
-        Misc.Thread_Start(self.Thread_Shell)
+        Misc.Thread_Start(self.Thread_Processor);
+        Misc.Thread_Start(self.Thread_Shell);
 
     def Thread_Shell(self) -> None:
         while (self.Connected and self.Interactive):
@@ -184,17 +148,34 @@ class Trinity_Client(Trinity_Socket):
 
 
 class Trinity_Ignition:
-    def __init__(self, Processor, Routine, Type: str = "Relay") -> None:
+    def __init__(self, Processor = None, Routine = None, Type: str = "Relay") -> None:
         self.Processor = Processor;
 
         match Type:
             case "Relay": # Relay
                 Log.Info("Starting Trinity Server as a Relay...")
+                self.Processor = self.Trinity_Relayer if (Processor == None) else Processor;
+                self.Routine = self.Trinity_Routine if (Routine == None) else Routine;
+
                 Log.Info("Loading Endpoints Configuration...");
-                Configuration = File.JSON_Read("Relay.json");
+                self.Configuration: dict = File.JSON_Read("Relay.json");
+                Log.Debug("Loading Nodes...")
+                self.Nodes: dict = {};
+                for Node in self.Configuration["Nodes"]:
+                    self.Nodes[Node] = {
+                        "Connected": False,
+                        "Socket": None,
+                    };
+                self.Node_Reconnect();
+
+                global Packet_Size, Server_Max_Communication_Errors, Server_Max_Communication_Length;
+                Log.Debug("Loading Relay Settings...")
+                Packet_Size = self.Configuration["Settings"]["Packet_Size"];
+                Server_Max_Communication_Errors = self.Configuration["Settings"]["Server_Max_Communication_Errors"];
+                Server_Max_Communication_Length = self.Configuration["Settings"]["Server_Max_Communication_Length"];
 
                 asyncio.run(self.WebSocket_Thread());
-                Routine();
+                self.Routine(self);
 
             case "Endpoint":
                 Log.Info("Starting Trinity Server as an Endpoint...")
@@ -205,15 +186,54 @@ class Trinity_Ignition:
             case _:
                 Log.Critical(f"Unknown Trinity Server Type: {self.Type}. Shutting down.")
                 quit();
+    
+    def Node_Reconnect(self) -> None:
+        Log.Debug("Reconnecting Nodes...")
+        for Node in self.Configuration["Nodes"]:
+            if (self.Nodes[Node]["Connected"] == False):
+                Misc.Thread_Start(
+                    self.Node_Thread,
+                    (Node, self.Configuration["Nodes"][Node]["Address"])
+                );
+
+    def Node_Thread(self, Node_Name: str, Node_Address: str) -> None:
+        try:
+            self.Nodes[Node_Name]["Socket"] = Trinity_Client(Address=Node_Address);
+            while (self.Nodes[Node_Name]["Socket"].Connected == True):
+                self.Nodes[Node_Name]["Connected"] = True;
+                time.sleep(60)
+        except Exception as Except:
+            Log.Critical(f"Node {Node_Name} might be down:\nEXCEPTION:\n\t{Except}")
+            Misc.Void();
+        self.Nodes[Node_Name]["Connected"] = False;
 
 
-    async def WebSocket_Thread(self, Port: int = 440) -> None:
+    async def WebSocket_Thread(self, Port: int = 8080) -> None:
         Log.Info("Started WebSockets Thread.");
         def Web_Threader(WebSocket) -> None:
-            Trinity_Server(self.Processor, WebSocket);
+            Connected_Clients.append(Trinity_Server(self.Processor, WebSocket));
 
-        with serve(Web_Threader, "0.0.0.0", 8080) as WS:
+        with serve(Web_Threader, "0.0.0.0", Port) as WS:
             WS.serve_forever();
+    
+    def Trinity_Relayer(self, Client: Trinity_Server, Command: str) -> bool:
+        Client.Send(Command);
+        return True;
+
+    def Trinity_Routine(self) -> None:
+        while (Server_Running):
+            Log.Carriage(f"Connected Clients: {len(Connected_Clients)} // {len(self.Nodes)} Nodes out of {len(self.Configuration["Nodes"])} Connected.");
+            time.sleep(1);
+            for Client in Connected_Clients:
+                if (not Client.Connected):
+                    Connected_Clients.remove(Client);
+            self.Node_Reconnect();
+
+
+
+Version: str = "a250330";
+Connected_Clients: list[Trinity_Server] = [];
+Server_Running: bool = True;
 
 # If the file is ran as is, assuming we want to start the Trinity Relay.
 if (__name__== "__main__"):
@@ -222,4 +242,4 @@ if (__name__== "__main__"):
     Config.Logging["File"] = True; # Allow Log Files
     Config.Logging["Print_Level"] = 0; # Show ALL messages
     dotenv.load_dotenv()
-    Trinity_Ignition(Processor=Trinity_Relayer, Routine=Trinity_Routine, Type="Relay");
+    Trinity_Ignition(Type="Relay");
