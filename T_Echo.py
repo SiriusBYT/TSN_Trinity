6c69cdfbb7f132dd64cce79683955059ec597efb
Ascellayn <root.siriusb@gmail.com>
2025-03-30 19:08:16 +0200

    this code is so shit and commit is monolithic i'll let the garbage copilot thingy do the commit message: Fix address formatting in Relay.json and update Echo function in T_Echo.py; add Endpoint.json configuration

--- T_Echo.py
+++ T_Echo.py
@@ -2,15 +2,19 @@
 from Trinity import *;
 
 # Example Processor
-def Echo(Client: Trinity_Socket, Command: str) -> bool:
+def Echo(Client: Trinity_Client, Command: str) -> bool:
     Client.Send(Command);
     return True;
 
 # Example Routine Function
-def Echo_Routine(Client: Trinity_Socket):
-    time.sleep(10);
-    while (Client.Connected):
-        Client.Send(f"Routine Message!");
-        time.sleep(10);
+def Echo_Routine():
+    while True:
+        time.sleep(1);
+        for Relay in Connected_Clients:
+            if (not Relay.Connected):
+                Connected_Clients.remove(Relay); # This causes issues I think
+        Log.Info(f"We can broadcast shid. is nice. {Connected_Clients}");
+        for Relay in Connected_Clients:
+            Relay.Send("There is a Mika hidden in your walls.")
 
-Trinity_Server(Processor=Echo, Routine=Echo_Routine, Type="Endpoint");
+Trinity_Ignition(Processor=Echo, Routine=Echo_Routine, Type="Endpoint");
--- Trinity.py
+++ Trinity.py
@@ -4,35 +4,42 @@ import websockets;
 from websockets.sync.server import serve;
 from websockets.sync.client import connect;
 
-import time, math, psutil;
+import time, math, psutil, uuid;
 
 class Trinity_Socket:
     def __init__(
             self, 
             Processor = None,
             WebSocket = None,
+            Config: dict = {},
             Address: str = "ws://localhost:8080",
-            Tickrate: int = 0.01,
             **kwargs: dict[str, dict]
         ):
         """NOTE: Processor is a FUNCTION! For some reason adding "function" to declare we want to accept a function,
         doesn't FUCKING WORK because Python is retarded or something. This is hateful. We're at the mercy of the user not fucking up."""
+        self.kwargs: dict = kwargs;
+
+        # Node Shenanigans (JANKY!!!)
+        self.ID: str = self.kwargs.get("ID");
+        if (self.ID != None): Log.Debug(f"This Socket was created with ID {self.ID}.");
+        if (self.kwargs.get("is_Node") == True): Connected_Nodes.append(self);
+
         self.Socket = WebSocket;
-        if (self.Socket != None):
+
+        if (self.Socket != None and Address == "ws://localhost:8080"):
             Connected_Clients.append(self);
+
         self.Address: str = f"{self.Socket.remote_address[0]}:{self.Socket.remote_address[1]}" if (WebSocket != None) else Address;
         self.Last_Sent: str = "";
 
         self.Processor = Processor;
-        self.Tickrate: int = Tickrate;
+        self.Config = Config;
 
         self.Communication_Errors: int = 0;
         self.Auth_Level: int = 0;
 
         self.Connected = self.Process_Auto = True;
         self.Queue: list[str] = [];
-
-        self.kwargs: dict = kwargs;
 
         self.Configuration();
         self.Thread_Receive();
@@ -44,7 +51,7 @@ class Trinity_Socket:
         """ Wait until the queue is no longer empty
         This function is blocking on purpose. """
         while (len(self.Queue) == 0):
-            time.sleep(self.Tickrate);
+            time.sleep(self.Config["Tick_Rate"]);
         return;
 
     def Configuration(self) -> None:
@@ -82,7 +89,7 @@ class Trinity_Socket:
     def Communication_Failed(self) -> None:
         Log.Debug(f"{self.Address} [!] {Log.Get_Caller(3)}()");
         self.Communication_Errors += 1;
-        if (self.Communication_Errors >= Server_Max_Communication_Errors):
+        if (self.Communication_Errors >= self.Config["Server_Max_Communication_Errors"]):
             self.Terminate();
 
     def Thread_Receive(self):
@@ -110,15 +117,17 @@ class Trinity_Socket:
                        if (self.Processor(self, self.Queue[0])):
                            self.Queue.pop(0);
                    else:
-                       time.sleep(self.Tickrate);
+                       time.sleep(self.Config["Tick_Rate"]);
             else:
-                time.sleep(self.Tickrate);
+                time.sleep(self.Config["Tick_Rate"]);
     
     def Thread_Timeout(self):
         # Terminate a client if they're connected for more than an hour by default.
-        time.sleep(Server_Max_Communication_Length);
+        time.sleep(self.Config["Server_Max_Communication_Length"]);
         self.Send_Code("CONNECTION_LENGTH_EXCEEDED");
         self.Terminate();
+
+
 
 class Trinity_Server(Trinity_Socket):
     def Configuration(self) -> None:
@@ -127,23 +136,22 @@ class Trinity_Server(Trinity_Socket):
         Misc.Thread_Start(self.Thread_Processor);
         Misc.Thread_Start(self.Thread_Timeout);
 
-
-
 class Trinity_Client(Trinity_Socket):
     def Configuration(self) -> None:
         self.Ping = time.monotonic()*1000;
         try:
             self.Socket = connect(self.Address);
-        except:
+        except Exception as Except:
+            Log.Critical(f"Exception while connecting to {self.Address}:\nEXCEPTION:\n\t{Except}");
             self.Connected: bool = False;
             return;
         self.Ping = math.floor(((time.monotonic()*1000) - self.Ping));
-        self.Interactive = self.kwargs["Shell"];
+        self.Interactive = self.kwargs.get("Shell");
         Log.Info(f"Connected to {self.Address} with a latency of {self.Ping}ms.");
 
         Misc.Thread_Start(self.Thread_Processor);
 
-        if (self.Interactive):
+        if (self.Interactive == True):
             Misc.Thread_Start(self.Thread_Shell);
 
     def Thread_Shell(self) -> None:
@@ -154,79 +162,127 @@ class Trinity_Client(Trinity_Socket):
 
     def Communication_Failed(self) -> None: return;
 
+class Trinity_Requester(Trinity_Socket):
+    def __init__(
+            self, 
+            Request: str,
+            Address: str = "ws://localhost:8080"
+        ):
+        self.Request: str = Request;
+        self.Address: str = Address;
+    
+    def Requester(self) -> str:
+        try:
+            self.Socket = connect(self.Address);
+            self.Send(self.Request);
+            for Request in self.Socket:
+                if (Request != ""):
+                    Log.Info(f"{self.Address} <- {Request}");
+                    return Request;
+        except:
+            return "CODE¤ENDPOINT_OFFLINE";
 
-
-
+    def Thread_Processor(self): return;
 
 class Trinity_Ignition:
     def __init__(self, Processor = None, Routine = None, Type: str = "Relay") -> None:
-        self.Processor = Processor;
+        self.Type: str = Type;
+
+        Log.Info(f"Starting Trinity Server as: {self.Type}")
+        self.Processor = self.Trinity_Relayer if (Processor == None) else Processor;
+        self.Routine = self.Trinity_Routine if (Routine == None) else Routine;
+
+        Log.Info(f"Loading {Type} Config");
+        self.Config: dict = File.JSON_Read(f"{self.Type}.json");
+        
+        Log.Debug(f"Loading {self.Type} Settings")
+        global Packet_Size, Tick_Rate, Server_Max_Communication_Errors, Server_Max_Communication_Length;
+        self.Address = self.Config["Settings"]["Address"];
+        self.Port = self.Config["Settings"]["Port"];
+        Packet_Size = self.Config["Settings"]["Packet_Size"];
+        Tick_Rate = self.Config["Settings"]["Tick_Rate"];
+        Server_Max_Communication_Errors = self.Config["Settings"]["Server_Max_Communication_Errors"];
+        Server_Max_Communication_Length = self.Config["Settings"]["Server_Max_Communication_Length"];
 
         match Type:
             case "Relay": # Relay
-                Log.Info("Starting Trinity Server as a Relay")
-                self.Processor = self.Trinity_Relayer if (Processor == None) else Processor;
-                self.Routine = self.Trinity_Routine if (Routine == None) else Routine;
-
-                Log.Info("Loading Endpoints Configuration");
-                self.Configuration: dict = File.JSON_Read("Relay.json");
                 Log.Debug("Loading Nodes")
                 self.Nodes: dict = {};
-                for Node in self.Configuration["Nodes"]:
+                for Node in self.Config["Nodes"]:
                     self.Nodes[Node] = {
                         "Connected": False,
-                        "Socket": None,
+                        "Heartbeat": "OFFLINE"
                     };
-
-                global Packet_Size, Server_Max_Communication_Errors, Server_Max_Communication_Length;
-                Log.Debug("Loading Relay Settings")
-                Packet_Size = self.Configuration["Settings"]["Packet_Size"];
-                Server_Max_Communication_Errors = self.Configuration["Settings"]["Server_Max_Communication_Errors"];
-                Server_Max_Communication_Length = self.Configuration["Settings"]["Server_Max_Communication_Length"];
 
                 Misc.Thread_Start(self.WebSocket_Thread);
                 self.Routine();
-
             case "Endpoint":
-                Log.Info("Starting Trinity Server as an Endpoint")
-                pass;
+                Misc.Thread_Start(self.WebSocket_Thread);
+                self.Routine();
             case "Heartbeat":
                 Log.Info("Starting Trinity Endpoint Heartbeat")
                 pass;
             case _:
                 Log.Critical(f"Unknown Trinity Server Type: {self.Type}. Shutting down.")
                 quit();
-    
+
+
+
     def Node_Reconnect(self) -> None:
         Log.Debug("Reconnecting Nodes")
-        for Node in self.Configuration["Nodes"]:
+        for Node in self.Config["Nodes"]:
             if (self.Nodes[Node]["Connected"] == False):
                 Log.Debug(f"Attempting to reestablish link with {Node}.")
                 Misc.Thread_Start(
                     self.Node_Thread,
-                    (Node, self.Configuration["Nodes"][Node]["Address"])
+                    (Node, self.Config["Nodes"][Node]["Address"])
                 );
         Log.Debug("Done attempting to reconnect nodes.")
 
     def Node_Thread(self, Node_Name: str, Node_Address: str) -> None:
+        def Node_Processor(Node: Trinity_Client, Command: str):
+            # Assume every Node Message is a broadcast to ALL clients.
+            for Client in Connected_Clients:
+                Client.Send(Command)
+        
+        def Node_Threader(Node_Address: str, Node_ID: str) -> None:
+            Trinity_Client(Processor=Node_Processor, Address=Node_Address, Config=self.Config["Settings"], ID=Node_ID, is_Node=True);
+        # This got to be the worst code I've ever written. Not even Mika would xd at this.
         try:
-            self.Nodes[Node_Name]["Socket"] = Trinity_Client(Address=Node_Address);
-            while (self.Nodes[Node_Name]["Socket"].Connected == True):
-                self.Nodes[Node_Name]["Connected"] = True;
-                time.sleep(60)
-        except: Misc.Void();
+            Node_Socket = None;
+            Node_ID = str(uuid.uuid4());
+            time.sleep(1); # Funny race condition
+            Misc.Thread_Start(Node_Threader, (Node_Address, Node_ID));
+            time.sleep(1); # Funny race condition
+            for Node in Connected_Nodes:
+                if (Node.ID == Node_ID):
+                    Node_Socket = Node;
+                    break;
+        except Exception as Except:
+            raise Exception(Except);
+    
+        if (Node_Socket == None):
+            raise Exception("Could not find ID in Connected Nodes!");
+    
+        while (Node_Socket.Connected == True):
+            self.Nodes[Node_Name]["Connected"] = True;
+            time.sleep(2);
+            Log.Debug(f"{Node_Name} Queue: {Node_Socket.Queue}");
         Log.Critical(f"Node {Node_Name} is down!")
         self.Nodes[Node_Name]["Connected"] = False;
 
 
-    def WebSocket_Thread(self, Port: int = 8080) -> None:
+
+    def WebSocket_Thread(self) -> None:
         Log.Info("Started WebSockets Thread.");
         def Web_Threader(WebSocket) -> None:
-            Trinity_Server(self.Processor, WebSocket);
+            Trinity_Server(self.Processor, WebSocket, self.Config["Settings"]);
 
-        with serve(Web_Threader, "0.0.0.0", Port) as WS:
+        with serve(Web_Threader, self.Address, self.Port) as WS:
             WS.serve_forever();
-    
+
+
+
     def Trinity_Relayer(self, Client: Trinity_Server, Command: str) -> bool:
         """
         True = Processed Request Successfully
@@ -252,14 +308,14 @@ class Trinity_Ignition:
                     Client.Send_Code("INVALID_COMMAND");
         else:
             if (Command[0] in self.Nodes):
-                Client.Send("TBD");
+                Client.Send(Trinity_Requester(Command[1], self.Config["Nodes"][Command[0]]["Address"]).Requester());
             else:
-                Client.Send_Code("UNKNOWN_ENDPOINT")
+                Client.Send_Code("ENDPOINT_UNKNOWN");
         return True;
 
     def Trinity_Routine(self) -> None:
-        Reconnect_Delay: int = 60;
-        Delay_Tick: int = 60;
+        Reconnect_Delay: int = 8;
+        Delay_Tick: int = 8;
         while (Server_Running):
             if (Delay_Tick == Reconnect_Delay):
                 Delay_Tick: int = 0
@@ -272,15 +328,15 @@ class Trinity_Ignition:
             for Client in Connected_Clients:
                 if (not Client.Connected):
                     Connected_Clients.remove(Client);
-            Log.Carriage(f"[Nodes: {Nodes_Connected}/{len(self.Configuration["Nodes"])}] - [Clients: {len(Connected_Clients)}]");
+            Log.Carriage(f"[Nodes: {Nodes_Connected}/{len(self.Config["Nodes"])}] - [Clients: {len(Connected_Clients)}]");
             time.sleep(1);
             Delay_Tick+=1;
-
 
 
 Trinity_Version: str = "a250330";
 Trinity_LUnix: int = Time.Get_Unix(); # Unix Time of when Server Launched, calculation of uptime is done client side.
 Connected_Clients: list[Trinity_Server] = [];
+Connected_Nodes: list[Trinity_Client] = [];
 Server_Running: bool = True;
 
 # If the file is ran as is, assuming we want to start the Trinity Relay.
